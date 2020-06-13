5ed2065dce950a433e08b2fff2feb7bbf3fcb55e
iacopo.mezzanotte <iacosmos98@gmail.com>
2020-06-13 12:46:42 +0200

    first push of siw's project

---
+++ taskmanager_3/src/main/java/it/uniroma3/siw/taskmanager/controller/UserController.java
@@ -0,0 +1,151 @@
+package it.uniroma3.siw.taskmanager.controller;
+
+import it.uniroma3.siw.taskmanager.controller.session.SessionData;
+import it.uniroma3.siw.taskmanager.controller.validation.CredentialsValidator;
+import it.uniroma3.siw.taskmanager.controller.validation.UserValidator;
+import it.uniroma3.siw.taskmanager.model.Credentials;
+import it.uniroma3.siw.taskmanager.model.User;
+import it.uniroma3.siw.taskmanager.repository.UserRepository;
+import it.uniroma3.siw.taskmanager.service.CredentialsService;
+import it.uniroma3.siw.taskmanager.service.UserService;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * The UserController handles all interactions involving User data.
+ */
+@Controller
+public class UserController {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	UserValidator userValidator;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	SessionData sessionData;
+
+	@Autowired
+	CredentialsService credentialsService;
+
+	@Autowired
+	CredentialsValidator credentialsValidator;
+	
+	@Autowired
+	UserService userService;
+
+	/**
+	 * This method is called when a GET request is sent by the user to URL "/users/user_id".
+	 * This method prepares and dispatches the User registration view.
+	 *
+	 * @param model the Request model
+	 * @return the name of the target view, that in this case is "register"
+	 */
+	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
+	public String home(Model model) {
+		User loggedUser = sessionData.getLoggedUser();
+		model.addAttribute("user", loggedUser);
+		return "home";
+	}
+
+	/**
+	 * This method is called when a GET request is sent by the user to URL "/users/user_id".
+	 * This method prepares and dispatches the User registration view.
+	 *
+	 * @param model the Request model
+	 * @return the name of the target view, that in this case is "register"
+	 */
+	@RequestMapping(value = { "/users/me" }, method = RequestMethod.GET)
+	public String me(Model model) {
+		User loggedUser = sessionData.getLoggedUser();
+		Credentials credentials = sessionData.getLoggedCredentials();
+		System.out.println(credentials.getPassword());
+		model.addAttribute("loggedUuser", loggedUser);
+		model.addAttribute("credentials", credentials);
+
+		return "userProfile";
+	}
+
+	/**
+	 * This method is called when a GET request is sent by the user to URL "/users/user_id".
+	 * This method prepares and dispatches the User registration view.
+	 *
+	 * @param model the Request model
+	 * @return the name of the target view, that in this case is "register"
+	 */
+	@RequestMapping(value = { "/admin" }, method = RequestMethod.GET)
+	public String admin(Model model) {
+		User loggedUser = sessionData.getLoggedUser();
+		model.addAttribute("user", loggedUser);
+		return "admin";
+	}
+
+	@RequestMapping(value = { "/admin/users" }, method = RequestMethod.GET)
+	public String usersList(Model model) {
+		User loggedUser = sessionData.getLoggedUser();
+		model.addAttribute("user", loggedUser);
+		List<Credentials> allCredentials = this.credentialsService.getAllCredentials();
+		model.addAttribute("user", loggedUser);
+		model.addAttribute("allCredentials", allCredentials );
+		return "allUsers";
+	}
+
+	@RequestMapping(value = { "/admin/users/{username}/delete" }, method = RequestMethod.POST)
+	public String removeUser(@PathVariable String username ,Model model) {
+		this.credentialsService.deleteCredentials(username);
+		return "redirect:/admin/users";
+	}
+	
+	@RequestMapping(value = {"/users/me/update"}, method = RequestMethod.GET)
+	public String userForm(Model model) {
+		User loggedUser = sessionData.getLoggedUser();
+		Credentials credentials = this.sessionData.getLoggedCredentials();
+		model.addAttribute("user", loggedUser);
+		model.addAttribute("credentials", credentials);
+		return "userUpdate";
+	}
+	
+	@RequestMapping(value = { "/users/me/update" }, method = RequestMethod.POST)
+	    public String registerUser(@Valid @ModelAttribute("user") User newUser,
+	                               BindingResult userBindingResult,
+	                               @Valid @ModelAttribute("credentials") Credentials newCredentials,
+	                               BindingResult credentialsBindingResult,
+	                               Model model) {
+
+	        // validate user and credentials fields
+	        this.userValidator.validate(newUser, userBindingResult);
+	        this.credentialsValidator.validate(newCredentials, credentialsBindingResult);
+
+	        // if neither of them had invalid contents, store the User and the Credentials into the DB
+	        if(!userBindingResult.hasErrors() && ! credentialsBindingResult.hasErrors()) {
+	            // set the user and store the credentials;
+	            // this also stores the User, thanks to Cascade.ALL policy
+	        	Credentials credentials = this.sessionData.getLoggedCredentials();
+	        	credentials = newCredentials;
+	        	credentials.setUser(newUser);
+	            credentialsService.saveCredentials(credentials);
+	            User loggedUser = sessionData.getLoggedUser();
+	            loggedUser = newUser;
+	            userService.saveUser(loggedUser);
+	            model.addAttribute("user", loggedUser);
+	            return "redirect:/home";
+	        }
+	        return "userUpdate";
+	    }
+}
---
+++ taskmanager_3/src/main/java/it/uniroma3/siw/taskmanager/model/Tag.java
@@ -0,0 +1,118 @@
+package it.uniroma3.siw.taskmanager.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Tag {
+	
+	@Id@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+
+	@Column(nullable=false) private String name;
+	@Column(nullable=false) private String color;
+	@Column(length=200) private String description;
+
+	@ManyToMany(mappedBy="myTags") private List<Task> myTasks;
+
+	public Tag() {
+		this.myTasks = new ArrayList<>();
+	}
+
+	public Tag(String name, String color , String description) {
+		this();
+		this.name = name;
+		this.color = color;
+		this.description = description;
+
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Task> getMyTasks() {
+		return myTasks;
+	}
+
+	public void setMyTasks(List<Task> myTasks) {
+		this.myTasks = myTasks;
+	}
+
+	public void addTask(Task task) {
+		if(!myTasks.contains(task))
+			this.myTasks.add(task);
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tag other = (Tag) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+}
